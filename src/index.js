0ad47f89bab34fbb151bc016a4258ea73501896e
Iggi <iggirex@gmail.com>
2018-03-05 09:46:59 -0700

    setting up React Router and fetching API data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+
+import PostsIndex from './components/posts_index';
 
 // ---- IMPORTING REACT ROUTER -----
 // Browswer Router -- interacts with History library and decides what to do on URL change
@@ -10,22 +13,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 
-import App from './components/app';
+// In the past we used App as our webpage, but with React Router we don't need a central single component 
+// import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() {
-    return <div>Hello!</div>
-  }
-}
-
-class Goodbye extends React.Component {
-  render() {
-    return <div>Goodbye!</div>
-  }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 //PATH property, --> user wants THIS route, then we will show this component
 // If user goes to hello path, show hello component, same with goodbye
@@ -33,8 +25,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     <div>
-      <Route path="/hello" component={Hello}/>  
-      <Route path="/goodbye" component={Goodbye}/>
+      <Route path="/" component={PostsIndex} />
     </div>
     </BrowserRouter>
   </Provider>
