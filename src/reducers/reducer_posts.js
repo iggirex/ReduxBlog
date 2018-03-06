aca60eec886ebea764542b002ce56b64fe2e6c66
Iggi <iggirex@gmail.com>
2018-03-06 15:02:26 -0700

    wrapped up blog

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,11 @@
 // dont need to specify "index.js", index is default below
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
     switch (action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         case FETCH_POST:
             const post = action.payload.data;
             // const newState { ...state, };   // NON ES6 way, ES6 way below!!
