43ceddc4f9d151d82ff63d23591009797d01c6c4
Iggi <iggirex@gmail.com>
2018-03-06 08:52:47 -0700

    showing individual post page

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -32,7 +32,9 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        this.props.createPost(values);
+        this.props.createPost(values, () =>{
+            this.props.history.push('/');
+        });
     }
 
     render() {
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
-import PostsNew from './components/posts_new';
 
+import PostsNew from './components/posts_new';
 import PostsIndex from './components/posts_index';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PostsShow from './components/posts_show';
 import reducers from './reducers';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +19,7 @@ ReactDOM.render(
     <div>
       <Switch>
         <Route path="/posts/new" component={PostsNew} />
+        <Route path="/posts/:id" component={PostsShow}></Route>
         <Route path="/" component={PostsIndex} />
       </Switch>
     </div>
